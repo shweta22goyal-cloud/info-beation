bc427093646e68d7304fb8e33b68e1f2981be119
Shweta Goyal <shweta.goyal@infobeans.com>
2026-04-21 15:54:00 +0530

    Clean git tracking with gitignore

---
+++ frontend/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect } from 'react';
+
+function App() {
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/test')
+    .then(res => res.json())
+    .then(data => {
+      console.log("data", data)
+    })
+
+  })
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
